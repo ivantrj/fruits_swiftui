f6b75af395eedb0b0cdc191cabe1fdfaf2d8829f
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-02-21 15:48:09 +0100

    Initial Commit

---
+++ Fruits/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fruits
+//
+//  Created by Ivan Trajanovski on 21.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Ivan Trajanovski on 21.02.24.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
